5459825680bfb03705de2213c03611efae46fd9a
eshvedov <eshvedov>
2024-12-18 10:12:12 +0300

    set fe config

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
 };
